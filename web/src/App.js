59bf2d7d71b93a7d388cc92bb9f31b29b5a9c1d4
Connor <conn.petri@gmail.com>
2025-04-07 15:03:23 -0700

    First Commit

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DictationComponent from './DictationComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DictationComponent />
+    </div>
+  );
+}
+
+export default App;
