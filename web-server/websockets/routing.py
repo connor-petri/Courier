089a78d413d6d781e9bfccffded7918bf7aa9855
Connor Petri <cpetri@printf.org>
2025-04-17 20:49:05 -0700

    Restructured web-server

--- web-server/websockets/routing.py
+++ web-server/websockets/routing.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .consumers.AudioStreamConsumer import AudioStreamConsumer
+import AudioStreamConsumer
 
 websocket_urlpatterns = [
-    path('ws/audio_stream/', AudioStreamConsumer.as_asgi()),
+    path('ws/audio-stream/', AudioStreamConsumer.as_asgi()),
 ]
 # This file defines the routing for the Django Channels application.
