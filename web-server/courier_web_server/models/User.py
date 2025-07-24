4be5031ccd9010bd2292a3b0b8f27ec992c649be
Connor Petri <cpetri@printf.org>
2025-07-23 21:04:38 -0700

    Removed Django server

--- web-server/courier_web_server/models/User.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    password = models.CharField(max_length=128)
-    
--- web-server/courier_web_server/websockets/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .AudioStreamConsumer import AudioStreamConsumer
-
-websocket_urlpatterns = [
-    path('ws/stream/', AudioStreamConsumer.as_asgi()),
-]
-# This file defines the routing for the Django Channels application.
